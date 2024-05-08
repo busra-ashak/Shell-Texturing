db3aacfaf9d664377b91632665b40edf98432461
busra-ashak <busr41s1k@gmail.com>
2024-05-08 20:09:38 +0200

    clear colour on every frame

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,27 @@ int main()
 {
 
     int err = glfwInit();
-    if(err==GLFW_FALSE)
+    if(err == GLFW_FALSE)
     {
         return -2;
     }
 
     GLFWwindow* window = glfwCreateWindow(640, 480, "Grass", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    int errGL = gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress));
+    if(errGL == 0)
+    {
+        return errGL;
+    }
+
     while(!glfwWindowShouldClose(window))
     {
+        glClear(GL_COLOR_BUFFER_BIT);
         glfwSwapBuffers(window);
         glfwPollEvents();
-    }    
+    }
+    
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
