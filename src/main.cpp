18646a156902795ef3f466bc3b5a1306c8ed2081
busra-ashak <busr41s1k@gmail.com>
2024-05-08 19:56:21 +0200

    create a window that works

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include <cstdio>
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
 
 int main()
 {
-    std::printf("Hello world");
+
+    int err = glfwInit();
+    if(err==GLFW_FALSE)
+    {
+        return -2;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Grass", NULL, NULL);
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }    
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
