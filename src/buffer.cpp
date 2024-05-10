ffc1dafc1c51f7372731d26acc7feb4851901712
busra-ashak <busr41s1k@gmail.com>
2024-05-10 21:41:43 +0200

    add write & bind buffer functions

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,4 +1,5 @@
 #include "buffer.h"
+#include <cstring>
 
 GLuint create_buffer(unsigned int size)
 {
@@ -11,4 +12,16 @@ GLuint create_buffer(unsigned int size)
 void destroy_buffer(GLuint buff)
 {
     glDeleteBuffers(1, &buff);
+}
+
+void write_buffer(GLuint buff, void* data, size_t size)
+{
+    void* ptr = glMapNamedBuffer(buff, GL_WRITE_ONLY);
+    memcpy(ptr, data, size);
+    glUnmapNamedBuffer(buff);
+}
+
+void bind_buffer(GLuint buff, GLuint slot)
+{
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, slot, buff);
 }
--- src/buffer.h
+++ src/buffer.h
@@ -2,3 +2,5 @@
 
 GLuint create_buffer(unsigned int size);
 void destroy_buffer(GLuint buff);
+void write_buffer(GLuint buff, void* data, size_t size);
+void bind_buffer(GLuint buff, GLuint slot);
